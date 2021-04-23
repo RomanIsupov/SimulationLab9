8ce0487030c19900ff1073ac0de20f187d82a4d7
RomanIsupov <romisup@gmail.com>
2021-04-23 23:40:23 +0800

    Fix naming mistakes.

--- src/main/java/com/example/simulationlab9/Experiment.java
+++ src/main/java/com/example/simulationlab9/Experiment.java
@@ -1,24 +1,59 @@
 package main.java.com.example.simulationlab9;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Experiment {
 
-    private int quantity;
-    private final double probability;
+    private int totalQuantity;
+    private final int eventsAmount = 5;
+    private final Event[] events;
 
-    public Experiment(double alpha) {
-        this.quantity = 0;
-        this.probability = alpha;
+    {
+        events = new Event[eventsAmount];
+        double alpha = 1.0;
+        for (int i = eventsAmount - 1; i > 0; i--) {
+            events[i] = new Event(alpha);
+            double probability = ThreadLocalRandom.current().nextDouble(0.0, alpha);
+            alpha -= probability;
+        }
+        events[0] = new Event(alpha);
     }
 
-    public void increaseQuantity() {
-        this.quantity++;
+    public Experiment(int totalQuantity) {
+        this.totalQuantity = totalQuantity;
     }
 
-    public double getProbability() {
-        return this.probability;
+    public void start() {
+        for (int i = 0; i < totalQuantity; i++) {
+            double alpha = ThreadLocalRandom.current().nextDouble(0.0, 1.0);
+            for (int j = 0; j < eventsAmount; j++) {
+                if (alpha < events[j].getProbability()) {
+                    events[j].increaseQuantity();
+                    break;
+                }
+            }
+        }
     }
 
-    public int getQuantity() {
-        return this.quantity;
+    public double[] getFrequencies() {
+        double[] frequencies = new double[eventsAmount];
+        for (int i = 0; i < eventsAmount; i++) {
+            frequencies[i] = (double) events[i].getQuantity() / (double)this.totalQuantity;
+        }
+        return frequencies;
+    }
+
+    public double[] getProbabilities() {
+        double[] probabilities = new double[eventsAmount];
+        double previous = 0.0;
+        for (int i = 0; i < eventsAmount; i++) {
+            probabilities[i] = events[i].getProbability() - previous;
+            previous = events[i].getProbability();
+        }
+        return probabilities;
+    }
+
+    public int getEventsAmount() {
+        return this.eventsAmount;
     }
 }
--- src/main/java/com/example/simulationlab9/Listener.java
+++ src/main/java/com/example/simulationlab9/Listener.java
@@ -9,24 +9,24 @@ public class Listener {
         System.out.println("Hello. Please enter the amount of events.");
         Scanner input = new Scanner(System.in);
         int quantity = input.nextInt();
-        Simulation simulation = new Simulation(quantity);
-        simulation.simulate();
-        Listener.printExperimentsData(simulation);
+        Experiment experiment = new Experiment(quantity);
+        experiment.start();
+        Listener.printExperimentsData(experiment);
     }
 
-    private static void printExperimentsData(Simulation simulation) {
+    private static void printExperimentsData(Experiment experiment) {
         DecimalFormat decimalFormat = new DecimalFormat("0.00000");
-        double[] probabilities = simulation.getProbabilities();
-        double[] frequencies = simulation.getFrequencies();
+        double[] probabilities = experiment.getProbabilities();
+        double[] frequencies = experiment.getFrequencies();
 
         System.out.println("Here are theoretical probabilities:");
-        for (int i = 0; i < simulation.getExperimentsAmount(); i++) {
+        for (int i = 0; i < experiment.getEventsAmount(); i++) {
             System.out.print(decimalFormat.format(probabilities[i]) + " ");
         }
         System.out.println();
 
         System.out.println("Here are empirical frequencies:");
-        for (int i = 0; i < simulation.getExperimentsAmount(); i++) {
+        for (int i = 0; i < experiment.getEventsAmount(); i++) {
             System.out.print(decimalFormat.format(frequencies[i]) + " ");
         }
     }
